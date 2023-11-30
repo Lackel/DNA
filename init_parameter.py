3bfcd466a60c0a3b2bebe932aff8764fb7c0573f
Lackel <787538412@qq.com>
2023-11-30 11:50:25 +0800

    initial commit

---
+++ init_parameter.py
@@ -0,0 +1,78 @@
+from argparse import ArgumentParser
+
+def init_model():
+    parser = ArgumentParser()
+    # data
+    parser.add_argument("--data_dir", default='data', type=str,
+                        help="The input data dir. Should contain the .csv files (or other data files) for the task.")
+    parser.add_argument("--dataset", default=None, type=str, required=True, 
+                        help="The name of the dataset to train selected.")
+    # model
+    parser.add_argument("--gpu_id", type=str, default='0', help="Select the GPU id.")
+    
+    parser.add_argument('--seed', type=int, default=0, help="Random seed for initialization.")
+
+    parser.add_argument("--model_name", default="../bert-base-uncased", type=str, help="The path or name of the pre-trained BERT model.")
+    
+    parser.add_argument("--freeze_bert_parameters", action="store_true", help="Freeze the last parameters of BERT.")
+    
+    parser.add_argument("--save_premodel", action="store_true", help="Save the pretrained model.")
+
+    parser.add_argument("--save_premodel_path", default="./premodel", type=str,
+                        help="Path to save the pretrained model checkpoints.")
+    
+    parser.add_argument("--save_model", action="store_true", help="Save the trained model.")
+
+    parser.add_argument("--save_model_path", default="./model", type=str,
+                        help="Path to save the trained model checkpoints.")
+    # hyperparameters
+    parser.add_argument("--feat_dim", default=768, type=int, help="The feature dimension.")
+
+    parser.add_argument("--warmup_proportion", default=0.1, type=float)
+
+    parser.add_argument("--m", default=5, type=int, help="The dimension for rank statistic constraint.")
+
+    parser.add_argument("--temperature", default=0.07, type=float, help="The temperature for contrastive learning.")
+
+    parser.add_argument("--alpha", default=0.99, type=float, help="The weighting factor for momentum negative key with different coarse labels as the query.")
+
+    parser.add_argument("--rtr_prob", default=0.25, type=float,
+                        help="Probability for random token replacement")
+
+    # training and testing
+    parser.add_argument("--train_batch_size", default=64, type=int,
+                        help="Batch size for training.")
+    
+    parser.add_argument("--pretrain_batch_size", default=64, type=int,
+                        help="Batch size for pre-training")
+    
+    parser.add_argument("--eval_batch_size", default=64, type=int,
+                        help="Batch size for evaluation.")
+
+    parser.add_argument("--num_train_epochs", default=20, type=float,
+                        help="The training epochs.")
+    
+    parser.add_argument("--num_pretrain_epochs", default=100, type=float,
+                        help="The pre-training epochs.")
+
+    parser.add_argument("--lr_pre", default=5e-5, type=float,
+                        help="The learning rate for pre-training.")
+    
+    parser.add_argument("--lr", default=5e-5, type=float,
+                        help="The learning rate for training.")  
+    
+    parser.add_argument("--wait_patient", default=20, type=int,
+                        help="Patient steps for Early Stop in pretraining.") 
+    
+    parser.add_argument("--view_strategy", default="rtr", type=str,
+                        help="Choose from rtr|shuffle|none")
+
+    parser.add_argument("--update_per_epoch", default=1, type=int,
+                        help="Update pseudo labels after certain amount of epochs")
+    
+    parser.add_argument("--topk", default=120, type=int,
+                        help="Select topk nearest neighbors")
+    
+    parser.add_argument("--grad_clip", default=1.0, type=float,
+                        help="Value for gradient clipping.")
+    return parser
